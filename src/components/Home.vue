9c3d6e338f4d51053309235f706438250007bd40
xiaolanhexiaolv <359286540@qq.com>
2018-12-27 16:07:34 +0800

    侧边栏功能完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,162 @@
 <template>
-<div>这是Home组件</div>
-
+  <el-container class="home">
+    <el-header>
+      <div class="logo"></div>
+      <div class="title">
+        <h1>电商后台管理系统</h1>
+      </div>
+      <div class="logout">
+        <span>欢迎光临</span>
+        <a href="javascript:;" @click="logout">退出</a>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          default-active="1-1"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          unique-opened
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="2-1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="2-2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="3-1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">商品列表</span>
+            </el-menu-item>
+            <el-menu-item index="3-2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">分类参数</span>
+            </el-menu-item>
+            <el-menu-item index="3-3">
+              <i class="el-icon-menu"></i>
+              <span slot="title">商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="4-1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="5-1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">数据报表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
-
 <script>
 export default {
-
+  methods: {
+    logout() {
+      this.$confirm('你确认退出登录吗', '温馨提示', {
+        type: 'warning'
+      })
+        .then(() => {
+          localStorage.removeItem('token')
+          this.$router.push('/login')
+          this.$message.success('退出成功')
+        })
+        .catch(() => {
+          this.$message.info('取消退出')
+        })
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.home {
+  height: 100%;
+  .el-header {
+    background-color: #b3c1cd;
+    display: flex;
+    .logo {
+      width: 180px;
+      background-image: url(~@/assets/logo.png);
+      background-size: contain;
+      background-repeat: no-repeat;
+    }
+    .title {
+      flex: 1;
+      h1 {
+        font-size: 25px;
+        text-align: center;
+        line-height: 60px;
+        color: #fff;
+      }
+    }
+    .logout {
+      line-height: 60px;
+      font-weight: 700;
+      text-align: right;
+      a {
+        color: orange;
+      }
+    }
+  }
+  .el-aside {
+    background-color: #545c64;
+    .el-submenu {
+      width: 200px;
+    }
+  }
+  .el-main {
+    background-color: #d4dfe4;
+    .logout {
+      width: 180px;
+    }
+  }
+}
 </style>
